b49cac7a89f40a56257efd1faf844120b665ed13
Sam Sheeran <ssheeran@yousifcapital.com>
2026-01-15 15:48:53 -0500

    created file structure

--- src/equity_reporting/config.py
+++ src/equity_reporting/config.py
@@ -8,4 +8,5 @@ load_dotenv()
 class DataPaths:
     BASE_PATH = Path(os.getenv("BASE_PATH", "C:/Users/DefaultUser/"))
     ONEDRIVE_PATH = Path(os.getenv("ONEDRIVE_PATH", BASE_PATH / "OneDrive"))
-
+    monthly_distributions=Path(os.getenv("ONEDRIVE_PATH"),"Portsys//reporting data//monthly distributions.xlsx")
+    dont_invest_dividends=Path(os.getenv("ONEDRIVE_PATH"),"Portsys//reporting data//dont invest dividends.xlsx")
